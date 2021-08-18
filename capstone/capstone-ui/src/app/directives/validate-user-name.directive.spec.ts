06686cd304558f0fe304dbe16b366e888b5aae38
venkatesh8 <venky88.iiitb@gmail.com>
2021-08-18 17:11:16 +0530

    Delete capstone/capstone-ui directory

--- capstone/capstone-ui/src/app/directives/validate-user-name.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidateUserNameDirective } from './validate-user-name.directive';
-
-describe('ValidateUserNameDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValidateUserNameDirective();
-    expect(directive).toBeTruthy();
-  });
-});
